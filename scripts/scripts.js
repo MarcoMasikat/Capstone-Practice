0c3ccde0525afc8ef8f199e2142928bf5c977000
Marco <s_marco.masikat@ousd.org>
2021-10-08 09:40:52 -0700

    Draft Capstone

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,3 +8,13 @@ $(function () {
     document.title = "Endangered Rhinos"
 
   })
+
+    $('#link').click(function () {
+        var src = 'http://www.youtube.com/v/FSi2fJALDyQ&amp;autoplay=1';
+        $('#myModal').modal('show');
+        $('#myModal iframe').attr('src', src);
+    });
+
+    $('#myModal button').click(function () {
+        $('#myModal iframe').removeAttr('src');
+    });
