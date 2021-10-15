c9259bf2409a6281f9ecb45af05cf0e84e5cd93f
MarcoMasikat <s_marco.masikat@ousd.org>
2021-10-15 00:01:07 -0700

    Capstone Practice

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,9 +8,3 @@ $(function () {
     document.title = "Endangered Rhinos"
 
   })
-
-  const randomNumber = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-
-  console.log(randomNumber(5,80));
