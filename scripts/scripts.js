17bbca2078035b7667af365ed56d9209269357d1
MarcoMasikat <s_marco.masikat@ousd.org>
2021-10-14 22:09:15 -0700

    Capstone Practice

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,13 +8,3 @@ $(function () {
     document.title = "Endangered Rhinos"
 
   })
-
-    $('#link').click(function () {
-        var src = 'http://www.youtube.com/v/FSi2fJALDyQ&amp;autohttps://www.youtube.com/watch?v=Q8eh58Z249o&t=306slay=1';
-        $('#myModal').modal('show');
-        $('#myModal iframe').attr('src', src);
-    });
-
-    $('#myModal button').click(function () {
-        $('#myModal iframe').removeAttr('src');
-    });
